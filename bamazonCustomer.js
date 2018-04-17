a7b16c6f3fc3a7a1ef412b8baf6e7d03f05bedef
Cezar Carvalhaes <cezarcarvalhaes@gmail.com>
2018-04-17 17:37:32 -0400

    completed bamazonCustomer.js

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -22,29 +22,66 @@ function customer() {
     connection.query("SELECT * FROM products", function (error, results) {
         if (error) throw error;
         //Print items for sale, then ask if the customer wants to buy something.
-        inquirer
-            .prompt([
-                {
-                    name: "products",
-                    type: "list",
-                    choices: function () {
-                        var productArr = [];
-                        for (var i = 0; i < results.length; i++) {
-                            productArr.push(results[i].product_name);
-                        }
-                        return productArr;
-                    },
-                    message: "Would you like to purchase one of our awesome products?"
-                },
+        console.log(`
+        
+            -------------------------------------Welcome to Bamazon!-------------------------------------
+                                
+                                                Check out our stock!
+        `)
+        for (var i = 0; i < results.length; i++) {
+            console.log(
+                `ID: ${results[i].item_id}    Item: ${results[i].product_name}     
+                        Price: $${results[i].price}
+                        `
+            )
+        }
+        inquirer.prompt([
                 {
                     name: "buy",
                     type: "input",
-                    message: "How many would you like?"
+                    message: "Please enter the ID number of the item you would like to purchase."
+                },
+                {
+                    name: "amount",
+                    type: "input",
+                    message: "Awesome. Now, how many would you like?"
                 }
             ]).then(function(answer){
-                console.log(answer);
+                var itemSelect;
+                for (var i = 0; i < results.length; i++) {
+                  if (results[i].item_id == answer.buy) {
+                    itemSelect = results[i];
+                  }
+
+                }
+                if (answer.amount <= itemSelect.stock_quantity) {
+                    var totalPrice = parseFloat(answer.amount) * itemSelect.price
+                    console.log(`Success! You are purchasing ${answer.amount} ${itemSelect.product_name} for a total of $${totalPrice}`);
+                    var newAmount = itemSelect.stock_quantity - answer.amount;
+                    depleteStock(newAmount, itemSelect.item_id);
+                    console.log(results)
+                }
+                else {
+                    console.log(`Sorry, we only have ${itemSelect.stock_quantity} left in stock!`)
+                }
             });
 
     });
 }
+
+function depleteStock(purchased, id){
+    connection.query (
+            "UPDATE products SET ? WHERE ?",[
+            {
+                stock_quantity: purchased
+            },
+            {
+                item_id: id
+            }
+            ],
+            function(error) {
+                if (error) throw error;
+            }
+    )
+};
 
